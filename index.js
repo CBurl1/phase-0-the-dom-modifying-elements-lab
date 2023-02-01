0ecfe818d610725ca1f72f47bf898634e5e69e1f
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-01-31 21:49:52 -0500

    commit tree b0262bbccc139c460ee5f322e9b2554086423818

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let main = document.getElementById("main")
+main.parentNode.removeChild(main)
+
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader);
+
+const victory = document.getElementById(victory)
+newHeader.setAttribute("id", "victory")
+victory.textContent = "Collin Burleigh is the champion"
